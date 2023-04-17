70fe0e73210ef8fe6d7e7517bc9170926cc0c3f5
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2023-04-17 16:22:58 -0300

    Add dependencies

--- init.lua
+++ init.lua
@@ -1,1 +1,5 @@
 require("msz")
+
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+require("neo-tree").focus()
--- lua/msz/plugins.lua
+++ lua/msz/plugins.lua
@@ -10,6 +10,9 @@ return require('packer').startup(function(use)
   -- Plenary
   use "nvim-lua/plenary.nvim"
 
+  -- Icons
+  use("nvim-tree/nvim-web-devicons")
+
   -- Treesitter
   use {
 	  'nvim-treesitter/nvim-treesitter',
@@ -17,7 +20,22 @@ return require('packer').startup(function(use)
 		  local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
 		  ts_update()
 	  end,
+    config = function()
+      require("msz.configs.treesitter")
+    end,
   }
+
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" })
+
+  -- Statusline
+  use({
+    "nvim-lualine/lualine.nvim",
+    event = "BufEnter",
+    config = function()
+    require("msz.configs.lualine")
+    end,
+    requires = { "nvim-web-devicons" },
+  })
 
   -- Telescope
   use {
@@ -25,4 +43,61 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  if packer_bootstrap then
+    require("packer").sync()
+  end
+
+  -- cmp: Autocomplete
+  use({
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    config = function()
+    require("msz.configs.cmp")
+    end,
+  })
+
+  -- File manager
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v2.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+	})
+
+	-- Terminal
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+		config = function()
+			require("msz.configs.toggleterm")
+		end,
+	})
+
+	-- Git
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("msz.configs.gitsigns")
+		end,
+	})
+
+	-- Markdown Preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+
+	-- Autopairs
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("msz.configs.autopairs")
+		end,
+	})
 end)
