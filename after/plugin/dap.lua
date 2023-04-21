b05c7b0d6ee1f1a6c287f3b5ec0ef013c5a4e2b7
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2023-04-21 10:38:28 -0300

    Fix dap.configurations.cs to array

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -2,15 +2,17 @@ local dap = require("dap")
 
 dap.adapters.coreclr = {
   type = 'executable',
-  command = '~/.local/share/nvim/mason/packages/netcoredbg/libexec/netcoredbg/netcoredbg',
+  command = '/home/msz/.local/share/nvim/mason/packages/netcoredbg/libexec/netcoredbg/netcoredbg',
   args = { '--interpreter=vscode' }
 }
 
 dap.configurations.cs = {
-  type = "coreclr",
-  name = "launch - netcoredbg",
-  request = "launch",
-  program = function ()
-   return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. '/bin/Debug/', 'file')
-  end
+  {
+    type = "coreclr",
+    name = "launch - netcoredbg",
+    request = "launch",
+    program = function ()
+      return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    end
+  }
 }
