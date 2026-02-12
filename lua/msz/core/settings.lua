5a8a03ff02996b1d2f256d447d26a87f0e817b57
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2026-02-11 22:53:47 -0300

    add(plugin): Avante.nvim;

--- lua/msz/core/settings.lua
+++ lua/msz/core/settings.lua
@@ -2,8 +2,8 @@ local global = vim.g
 local o = vim.o
 
 -- <leader>
-global.mapleader = ' '
-global.maplocalleader = ' '
+global.mapleader = " "
+global.maplocalleader = " "
 
 -- line numbers
 o.number = true
@@ -20,18 +20,18 @@ o.breakindent = true
 -- search
 o.ignorecase = true
 o.smartcase = true
-o.tagcase = 'followscs'
+o.tagcase = "followscs"
 
 -- cursor
 o.cursorline = true
 o.cursorbind = false
 o.guicursor =
-'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- appearance
 o.termguicolors = true
-o.signcolumn = 'yes:2'
-o.syntax = 'on'
+o.signcolumn = "yes:2"
+o.syntax = "on"
 o.ruler = true
 
 -- buffer
@@ -39,29 +39,29 @@ o.title = true
 o.hidden = true
 
 -- words
-vim.opt.iskeyword:append('-')
-global.backspace = 'indent,eol,start,nostop'
-o.clipboard = 'unnamedplus'
+vim.opt.iskeyword:append("-")
+global.backspace = "indent,eol,start,nostop"
+o.clipboard = "unnamedplus"
 
 -- split window
 o.splitright = true
 global.splitbellow = true
-o.splitkeep = 'screen'
+o.splitkeep = "screen"
 o.startofline = true
 
 -- navigation
 o.scrolloff = 3
-global.mouse = 'a'
+global.mouse = "a"
 
 -- file
 o.swapfile = false
-global.encoding = 'utf-8'
+global.encoding = "utf-8"
 
 -- command bar
 global.wildmenu = true
 o.cmdheight = 0
 global.showcmd = false
-global.showcmdloc = 'statusline'
+global.showcmdloc = "statusline"
 
 -- edditing
 global.ttimeout = true
@@ -74,6 +74,9 @@ global.showmatch = true
 global.incsearch = true
 
 -- fold
-o.foldmethod = 'expr'
-o.foldexpr = 'nvim_treesitter#foldexpr()'
+o.foldmethod = "expr"
+o.foldexpr = "nvim_treesitter#foldexpr()"
 o.foldlevel = 4
+
+-- ai
+vim.opt.laststatus = 3
