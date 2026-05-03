19ae9d7923d97522c7ad7e4b4d5a7f1be685bbd9
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2026-05-03 17:22:53 -0300

    changes(interface): add rounded borders;

--- lua/msz/configs/cmp.lua
+++ lua/msz/configs/cmp.lua
@@ -1,88 +1,96 @@
 local luasnip = require("luasnip")
 local cmp = require("cmp")
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-require('luasnip.loaders.from_vscode').lazy_load()
+require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
-    { name = 'buffer' },
-    { name = 'bufname' },
-    { name = 'buffer-lines' },
-    { name = 'calc' },
-    { name = 'nvim_lsp_signature_help' },
-    { name = 'diag-codes',             options = { in_comment = true } },
-    { name = 'path' },
-    { name = 'async_path' },
-    { name = 'rg' },
-    { name = 'emoji' },
-    { name = 'nerdfont' },
-    { name = 'dotenv' },
-    { name = 'lazydev',                group_index = 0 }
-  }),
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+		{ name = "bufname" },
+		{ name = "buffer-lines" },
+		{ name = "calc" },
+		{ name = "nvim_lsp_signature_help" },
+		{ name = "diag-codes", options = { in_comment = true } },
+		{ name = "path" },
+		{ name = "async_path" },
+		{ name = "rg" },
+		{ name = "emoji" },
+		{ name = "nerdfont" },
+		{ name = "dotenv" },
+		{ name = "lazydev", group_index = 0 },
+	}),
+	window = {
+		completion = cmp.config.window.bordered({ border = "rounded" }),
+		documentation = cmp.config.window.bordered({ border = "rounded" }),
+	},
 })
 
-cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' },
-  },
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+	window = {
+		completion = cmp.config.window.bordered({ border = "rounded" }),
+		documentation = cmp.config.window.bordered({ border = "rounded" }),
+	},
 })
 
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' },
-  }, {
-    { name = 'cmdline' },
-  }),
-  matching = {
-    disallow_symbol_nonprefix_matching = false,
-    disallow_fullfuzzy_matching = false,
-    disallow_partial_fuzzy_matching = false,
-    disallow_fuzzy_matching = false,
-    disallow_partial_matching = false,
-    disallow_prefix_unmatching = false
-  },
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+	window = {
+		completion = cmp.config.window.bordered({ border = "rounded" }),
+		documentation = cmp.config.window.bordered({ border = "rounded" }),
+	},
+	matching = {
+		disallow_symbol_nonprefix_matching = false,
+		disallow_fullfuzzy_matching = false,
+		disallow_partial_fuzzy_matching = false,
+		disallow_fuzzy_matching = false,
+		disallow_partial_matching = false,
+		disallow_prefix_unmatching = false,
+	},
 })
 
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
